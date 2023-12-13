000dcab91de76a25b76be08ae3a1ef739c86b9cb
shylan14 <shilan2015n@gmail.com>
2023-12-13 14:26:03 +0200

    Create Lab5_5.py

---
+++ Lab5_5.py
@@ -0,0 +1,7 @@
+def calculate_rectangle_area(height, length):
+    return height * length
+
+height = 4
+length = 6
+area = calculate_rectangle_area(height, length)
+print(f"Площа прямокутника: {area}")
